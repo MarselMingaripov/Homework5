c5fe7117b3f7dc4643a17564881d38df974e961e
MarselMingaripov <bard1812@gmail.com>
2022-11-29 20:29:27 +0300

    выполнил задание 2

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,17 @@ public class Main {
     public static void main(String[] args) {
 
         boolean clientOS = true;
+        short clientDeviceYear = 2015;
         if (clientOS)
             System.out.println("Установите версию приложения для Android по ссылке");
         else System.out.println("Установите версию приложения для iOS по ссылке");
 
-
+        if (clientDeviceYear >= 2015){
+            if (clientOS)
+                System.out.println("Установите версию приложения для Android по ссылке");
+            else System.out.println("Установите версию приложения для iOS по ссылке");}
+        else if (clientOS)
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            else System.out.println("Установите облегченную версию приложения для iOS по ссылке");
     }
 }
