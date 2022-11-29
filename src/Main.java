f04a5a0be55ba3698bb462dfa5859abe95034bec
MarselMingaripov <bard1812@gmail.com>
2022-11-29 21:04:14 +0300

    выполнил задание 3

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,15 @@ public class Main {
         else if (clientOS)
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
             else System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+
+        int year = 2021;
+        if (year % 4 == 0){
+            if (year % 100 == 0){
+                if (year % 400 == 0)
+                    System.out.println(year + " является високосным");
+                else System.out.println(year + " не является високосным");}
+            else System.out.println(year + " является високосным");}
+        else System.out.println(year + " не является високосным");
+
     }
 }
