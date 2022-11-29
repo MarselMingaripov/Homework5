237c8f6225a404db1223f0ef9f6bb35fd526c554
MarselMingaripov <bard1812@gmail.com>
2022-11-29 20:18:58 +0300

    выполнил задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        boolean clientOS = true;
+        if (clientOS)
+            System.out.println("Установите версию приложения для Android по ссылке");
+        else System.out.println("Установите версию приложения для iOS по ссылке");
+
+
     }
 }
